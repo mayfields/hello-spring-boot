dd3ada91973847fc34eec74134ee81ee1621bcfc
mds <dingshengmao@gmail.com>
2019-04-18 15:56:09 +0800

    hello-spring-boot

---
+++ src/main/java/com/mdsword/hello/spring/boot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.mdsword.hello.spring.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String sayHi() {
+        return "Hello Spring Boot";
+    }
+
+}
---
+++ src/main/java/com/mdsword/hello/spring/boot/entity/PersonBean.java
@@ -0,0 +1,22 @@
+package com.mdsword.hello.spring.boot.entity;
+
+public class PersonBean {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
