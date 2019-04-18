bb88555f48d05368412ea8ed2c9a6416396d04de
mds <dingshengmao@gmail.com>
2019-04-18 16:09:15 +0800

    hello-spring-boot

--- src/main/java/com/mdsword/hello/spring/boot/controller/HelloController.java
+++ src/main/java/com/mdsword/hello/spring/boot/controller/HelloController.java
@@ -1,15 +1,14 @@
 package com.mdsword.hello.spring.boot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @GetMapping("")
     public String sayHi() {
-        return "Hello Spring Boot";
+        return "Hello Spring Boot ";
     }
 
 }
